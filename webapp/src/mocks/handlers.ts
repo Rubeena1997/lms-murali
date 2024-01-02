a0a2054af364380fae742efbec753d625e73453d
Murali Krishna Alakuntla <118282852+muralialakuntla3@users.noreply.github.com>
2024-01-02 15:28:30 +0530

    for env variable Updated handlers.ts

--- webapp/src/mocks/handlers.ts
+++ webapp/src/mocks/handlers.ts
@@ -1,18 +1,21 @@
+// handlers.ts file
 import { rest } from "msw";
 
 // Mock Data
 import { courseData, courses } from "./mock-data/courses";
 
+const baseURL = process.env.VITE_API_URL || 'http://localhost:8080/api';
+
 // Define handlers that catch the corresponding requests and returns the mock data.
 export const handlers = [
   rest.get(
-    `${import.meta.env.VITE_API_URL}/student/courses`,
+    `${baseURL}/student/courses`,
     (req, res, ctx) => {
       return res(ctx.status(200), ctx.json(courses));
     }
   ),
   rest.get(
-    `${import.meta.env.VITE_API_URL}/student/getCourse/:id`,
+    `${baseURL}/student/getCourse/:id`,
     (req, res, ctx) => {
       return res(ctx.status(200), ctx.json(courseData));
     }
