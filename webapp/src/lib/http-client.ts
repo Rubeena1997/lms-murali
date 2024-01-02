9e939f899390a3b60841f814b4228ad8289c0a4b
Murali Krishna Alakuntla <118282852+muralialakuntla3@users.noreply.github.com>
2024-01-02 15:29:24 +0530

    for env variable Updated http-client.ts

--- webapp/src/lib/http-client.ts
+++ webapp/src/lib/http-client.ts
@@ -1,7 +1,10 @@
+// http-client.ts file
 import axios from "axios";
 
+const VITE_API_URL = process.env.VITE_API_URL || 'http://localhost:8080/api';
+
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: VITE_API_URL,
 });
 
 export default axiosInstance;
