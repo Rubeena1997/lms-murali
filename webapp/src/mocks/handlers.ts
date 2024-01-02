c13fb33aa9c6a42769cbf59e293065cd10a4815c
Murali Krishna Alakuntla <118282852+muralialakuntla3@users.noreply.github.com>
2024-01-02 15:57:31 +0530

    Update handlers.ts

--- webapp/src/mocks/handlers.ts
+++ webapp/src/mocks/handlers.ts
@@ -1,12 +1,8 @@
-// handlers.ts file
 import { rest } from "msw";
-
-// Mock Data
 import { courseData, courses } from "./mock-data/courses";
 
 const baseURL = process.env.VITE_API_URL || 'http://localhost:8080/api';
 
-// Define handlers that catch the corresponding requests and returns the mock data.
 export const handlers = [
   rest.get(
     `${baseURL}/student/courses`,
